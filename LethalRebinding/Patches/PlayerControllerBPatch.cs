7e702fbf72207500380ab309b2f379ad6b24b166
legoandmars <legoandmars@gmail.com>
2023-11-21 23:42:30 -0800

    Initial commit

---
+++ LethalRebinding/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,49 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace LethalRebinding.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB), "Awake")]
+    internal class PlayerControllerBPatch
+    {
+        private static List<PlayerControllerB>? _instances = new();
+        private static MethodInfo? _onEnable;
+        private static MethodInfo? _onDisable;
+
+        public static void ApplyNewBindings(string bindings)
+        {
+            foreach(var instance in _instances)
+            {
+                if (instance == null) continue;
+                instance.playerActions.LoadBindingOverridesFromJson(bindings, true);
+            }
+            // TODO: Other null/exist checks (or check game state) to make sure this doesn't break anything?
+            // if (_instance == null) return;
+            // _onDisable?.Invoke(_instance, new object[] { });
+            /*_instance.playerActions = new();
+            _instance.playerActions.LoadBindingOverridesFromJson(bindings, true);
+            _onEnable?.Invoke(_instance, new object[] { });*/
+            Debug.Log("Applying new bindings!");
+            Debug.Log(bindings);
+        }
+
+        private static void Postfix(PlayerControllerB __instance)
+        {
+            _instances.Add(__instance);
+            if (_onEnable == null) _onEnable = AccessTools.Method(typeof(PlayerControllerB), "OnEnable");
+            if (_onDisable == null) _onDisable = AccessTools.Method(typeof(PlayerControllerB), "OnDisable");
+
+            // apply bindings to actions
+            // hope this doesn't break anything lol
+            ApplyNewBindings(IngamePlayerSettings.Instance.settings.keyBindings);
+        }
+    }
+}
---
+++ LethalRebinding/Patches/RebindEventPatch.cs
@@ -0,0 +1,28 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LethalRebinding.Patches
+{
+    [HarmonyPatch(typeof(IngamePlayerSettings), nameof(IngamePlayerSettings.ResetSettingsToDefault))]
+    internal class RebindEventPatch
+    {
+        private static void Postfix(IngamePlayerSettings __instance)
+        {
+            PlayerControllerBPatch.ApplyNewBindings(__instance.settings.keyBindings);
+        }
+    }
+
+    [HarmonyPatch(typeof(IngamePlayerSettings), nameof(IngamePlayerSettings.SaveChangedSettings))]
+    internal class RebindEventPatch2
+    {
+        private static void Postfix(IngamePlayerSettings __instance)
+        {
+            PlayerControllerBPatch.ApplyNewBindings(__instance.settings.keyBindings);
+        }
+    }
+}
