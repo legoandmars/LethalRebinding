5c45c9cb6f382c67ad64e118f87cb50d714e5615
legoandmars <legoandmars@gmail.com>
2023-11-24 12:33:10 -0800

    hackpatch terminal bug

---
+++ LethalRebinding/Patches/TerminalPatch.cs
@@ -0,0 +1,32 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LethalRebinding.Patches
+{
+    [HarmonyPatch(typeof(Terminal))]
+    internal class TerminalPatch
+    {
+        [HarmonyPatch(nameof(Terminal.QuitTerminal))]
+        [HarmonyPostfix]
+        private static void EndPostfix(Terminal __instance)
+        {
+            PlayerControllerB localPlayerController = GameNetworkManager.Instance.localPlayerController;
+            localPlayerController.inTerminalMenu = true;
+            DelayEnablingPausing(localPlayerController);
+        }
+
+        // dude WHAT
+        // WHY IS THIS NECESSARY???????????????
+        private static async void DelayEnablingPausing(PlayerControllerB localPlayerController)
+        {
+            await Task.Delay(100);
+            localPlayerController.inTerminalMenu = false;
+        }
+    }
+}
